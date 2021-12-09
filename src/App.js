5d9a55559ec97c7b32eff2bf44c865887e503b9a
Neha Aggarwal <neha.aggarwal@bbc.co.uk>
2021-12-09 22:24:17 +0000

    add serach functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {  Input, Button, Segment, Menu, Container } from 'semantic-ui-react';
 
-function App() {
+const App=()=> {
+  const [searchValue, setSearchValue] = useState('');
+  const [data, setData]= useState([])
+
+   async function handleSubmit(e){
+     e.preventDefault();
+     console.log("handlesubmit", searchValue);
+     const res = await fetch("http://localhost:8080/api/search", {
+      body: JSON.stringify({
+        searchTerm: searchValue
+      }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const response = await res.json();
+    setData(response);
+    (console.log("data", data));
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu inverted>
+        <Container ><Menu.Item name='Simple Search Service' />
+        </Container>
+      </Menu>
+      <Segment >
+        <Input type="search" placeholder="Search" value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
+        <Button type="submit" onClick={handleSubmit}>Search</Button>
+      </Segment>  
     </div>
   );
 }
